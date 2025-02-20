b2154d9676cb35beb3f10c871f0ea8003b7732a5
JiSeong Lee <cocoquiet@knu.ac.kr>
2025-02-20 23:26:39 +0900

    Feat: Impl Body

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Body {
+    m: f64,
+    px: f64,
+    py: f64,
+    vx: f64,
+    vy: f64,
+}
+
+impl Body {
+    fn new(m: f64, px: f64, py: f64, vx: f64, vy: f64) -> Self {
+        Body {
+            m: m,
+            px: px,
+            py: py,
+            vx: vx,
+            vy: vy,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
